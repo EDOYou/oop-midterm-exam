2466a3258a9639ed04871a08e1a47dee0b2a6dd3
Taghiyev Kanan <ktaghiyev01@gmail.com>
2023-04-21 18:36:02 +0200

    First commit Added files are the same as it was in the exam (this same code can be shown on Teams)

---
+++ src/main/c#/Enu.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextFile;
+
+namespace oop_midterm
+{
+    struct Database
+    {
+        public string id;
+        public int extraWorkHrs;
+        public int workHrsSum;
+    }
+
+    internal class Enu
+    {
+        private enum Status
+        {
+            abnorm,
+            norm
+        }
+
+        private TextFileReader fileReader;
+        public Database current;
+        private string row;
+        Status status;
+        bool end;
+
+        public Enu(string filename)
+        {
+            fileReader = new TextFileReader(filename);
+        }
+
+        private void Read()
+        {
+            status = fileReader.ReadLine(out row) ? Status.norm : Status.abnorm;
+        }
+
+        public void First()
+        {
+            Read();
+            Next();
+        }
+
+        public bool End()
+        {
+            return end;
+        }
+
+        public void Next()
+        {
+            end = Status.abnorm == status;
+
+            if (!end)
+            {
+                string[] data = row.Split(' ');
+                current.id = data[0];
+                current.extraWorkHrs = int.Parse(data[1]);
+
+                //Console.WriteLine(current.extraWorkHrs);
+                while (status == Status.norm && row.Split(' ')[0].Equals(current.id))
+                {
+                    //Console.WriteLine($"{current.id} {current.extraWorkHrs}");
+                    //Console.WriteLine(row);
+                    //Console.WriteLine(current.extraWorkHrs);
+                    //Console.WriteLine($"{current.id} {current.extraWorkHrs}");
+                    current.workHrsSum += int.Parse(row.Split(' ')[1]);
+                    Read();
+                }
+            }
+        }
+    }
+}
---
+++ src/main/c#/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace oop_midterm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool fileError = true;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Enter the file name: ");
+                    string filename = Console.ReadLine();
+                    Enu e = new Enu(filename);
+
+                    e.First();
+                    int max = 0;
+                    while (!e.End())
+                    {
+                        //Console.WriteLine(max);
+                        if (max < e.current.workHrsSum)
+                        {
+                            max = e.current.workHrsSum;
+                            //Console.WriteLine(e.current.w);
+                            //maxID = e.current.id;
+                        }
+                        Console.WriteLine(max);
+                        e.Next();
+                    }
+                    //Console.WriteLine($"{maxID} -> {max} hours");
+                    fileError = false;
+                }
+                catch (System.IO.FileNotFoundException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("\nFile not found.\n");
+                }
+            } while (fileError);
+        }
+    }
+}
